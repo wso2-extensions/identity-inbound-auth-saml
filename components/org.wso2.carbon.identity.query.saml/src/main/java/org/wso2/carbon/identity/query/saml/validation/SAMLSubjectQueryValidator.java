c2b7675f20d6a76b4009fd4b4bc04218ded00afb
gayangithub <gayanliyanaarachchi@gmail.com>
2016-09-13 06:45:11 +0530

    check user availability

--- components/org.wso2.carbon.identity.query.saml/src/main/java/org/wso2/carbon/identity/query/saml/validation/SAMLSubjectQueryValidator.java
+++ components/org.wso2.carbon.identity.query.saml/src/main/java/org/wso2/carbon/identity/query/saml/validation/SAMLSubjectQueryValidator.java
@@ -23,9 +23,13 @@ import org.apache.commons.logging.LogFactory;
 import org.opensaml.saml.saml2.core.RequestAbstractType;
 import org.opensaml.saml.saml2.core.Subject;
 import org.opensaml.saml.saml2.core.impl.SubjectQueryImpl;
+import org.wso2.carbon.context.CarbonContext;
 import org.wso2.carbon.identity.query.saml.dto.InvalidItemDTO;
 import org.wso2.carbon.identity.query.saml.exception.IdentitySAML2QueryException;
+import org.wso2.carbon.identity.query.saml.internal.SAMLQueryServiceComponent;
 import org.wso2.carbon.identity.query.saml.util.SAMLQueryRequestConstants;
+import org.wso2.carbon.user.api.UserStoreException;
+import org.wso2.carbon.user.api.UserStoreManager;
 
 import java.util.List;
 
@@ -45,7 +49,7 @@ public class SAMLSubjectQueryValidator extends AbstractSAMLQueryValidator {
      * @param invalidItems List of invalid items tracked by validation process
      * @param request      Any type of assertion request
      * @return Boolean true, if request message contain no validation errors
-     * @throws  IdentitySAML2QueryException If unable to validate SubjectQuery
+     * @throws IdentitySAML2QueryException If unable to validate SubjectQuery
      */
     @Override
     public boolean validate(List<InvalidItemDTO> invalidItems, RequestAbstractType request)
@@ -73,18 +77,30 @@ public class SAMLSubjectQueryValidator extends AbstractSAMLQueryValidator {
      * @param subjectQuery SubjectQuery request message
      * @return Boolean true, if request subject is valid
      */
-    protected boolean validateSubject(SubjectQueryImpl subjectQuery) {
+    protected boolean validateSubject(SubjectQueryImpl subjectQuery) throws IdentitySAML2QueryException {
         Subject subject = subjectQuery.getSubject();
         boolean isValidsubject = false;
+        try {
+            if (subject != null && subject.getNameID() != null &&
+                    subject.getNameID().getFormat() != null && super.getSsoIdpConfig().getNameIDFormat() != null &&
+                    subject.getNameID().getFormat().equals(super.getSsoIdpConfig().getNameIDFormat())) {
+                UserStoreManager userStoreManager = SAMLQueryServiceComponent.getRealmservice().
+                        getTenantUserRealm(CarbonContext.getThreadLocalCarbonContext().getTenantId()).
+                        getUserStoreManager();
+                String user = subject.getNameID().getValue();
+                if (userStoreManager.isExistingUser(user)) {
+                    log.debug("Request with id:" + subjectQuery.getID() + " contain valid subject");
+                    isValidsubject = true;
+                } else {
+                    log.debug("Request message subject :" + user + " is invalid");
+                }
 
-        if (subject != null && subject.getNameID() != null &&
-                subject.getNameID().getFormat() != null && super.getSsoIdpConfig().getNameIDFormat() != null &&
-                subject.getNameID().getFormat().equals(super.getSsoIdpConfig().getNameIDFormat())) {
-            log.debug("Request with id:" + subjectQuery.getID() + " contain valid subject");
-            isValidsubject = true;
-        } else {
-            log.debug("Request with id:" + subjectQuery.getID() + " contain in-valid subject");
-            return isValidsubject;
+            } else {
+                log.debug("Request with id:" + subjectQuery.getID() + " contain in-valid subject");
+                return isValidsubject;
+            }
+        }  catch (UserStoreException e) {
+            log.error("Unable to collect requested subject from user store",e);
         }
         return isValidsubject;
     }
