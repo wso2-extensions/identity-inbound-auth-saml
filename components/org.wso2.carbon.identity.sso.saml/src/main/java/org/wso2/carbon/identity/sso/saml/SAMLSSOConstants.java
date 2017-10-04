3d00ab37587f14bfb4af140e8ed153cb77d514c2
thanujalk <jatljayasinghe@gmail.com>
2017-10-04 18:08:06 +0530

    IDENTITY-5861: Fix passing AuthnContextClassRef returned from the IdP to the SP

--- components/org.wso2.carbon.identity.sso.saml/src/main/java/org/wso2/carbon/identity/sso/saml/SAMLSSOConstants.java
+++ components/org.wso2.carbon.identity.sso.saml/src/main/java/org/wso2/carbon/identity/sso/saml/SAMLSSOConstants.java
@@ -73,6 +73,8 @@ public class SAMLSSOConstants {
     public static final String COM_PROTOCOL = "https";
     public static final String CRYPTO_PROTOCOL = "TLS";
 
+    public static final String AUTHN_CONTEXT_CLASS_REF = "AuthnContextClassRef";
+
     private SAMLSSOConstants() {
     }
 
