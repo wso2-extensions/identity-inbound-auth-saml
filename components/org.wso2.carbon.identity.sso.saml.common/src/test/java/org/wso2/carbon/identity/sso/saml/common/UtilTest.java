b10d3dd5347b19c525378b641cdc0b7d179bd00a
hasinthaindrajee <hasintha@wso2.com>
2017-10-12 10:42:44 +0530

    adding initial tests for saml.common

---
+++ components/org.wso2.carbon.identity.sso.saml.common/src/test/java/org/wso2/carbon/identity/sso/saml/common/UtilTest.java
@@ -0,0 +1,50 @@
+package org.wso2.carbon.identity.sso.saml.common;
+
+import org.powermock.modules.testng.PowerMockTestCase;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class UtilTest extends PowerMockTestCase {
+
+    @Test
+    public void testGetSingleLogoutRetryCount() throws Exception {
+        assertTrue(true);
+    }
+
+    public void testSetSingleLogoutRetryCount() throws Exception {
+
+    }
+
+    public void testGetSingleLogoutRetryInterval() throws Exception {
+
+    }
+
+    public void testSetSingleLogoutRetryInterval() throws Exception {
+
+    }
+
+    public void testIsHttpSuccessStatusCode() throws Exception {
+
+    }
+
+    public void testDoPaging() throws Exception {
+
+    }
+
+    public void testDoFilter() throws Exception {
+
+    }
+
+    public void testGetUserNameFromOpenID() throws Exception {
+
+    }
+
+    public void testGetOpenID() throws Exception {
+
+    }
+
+    public void testGenerateOpenID() throws Exception {
+
+    }
+}
