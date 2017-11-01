1daf0f5354584aaa0b2df45ee48fcb08bff3a82b
Sugirjan <ragusugir@gmail.com>
2017-11-01 14:59:17 +0530

    add required changes

--- components/org.wso2.carbon.identity.sso.saml/src/main/java/org/wso2/carbon/identity/sso/saml/SAMLLogoutListener.java
+++ components/org.wso2.carbon.identity.sso.saml/src/main/java/org/wso2/carbon/identity/sso/saml/SAMLLogoutListener.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wso2.carbon.identity.sso.saml;
 
 import org.apache.commons.lang.StringUtils;
@@ -12,6 +28,10 @@ import org.wso2.carbon.identity.event.handler.AbstractEventHandler;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+
+/**
+ * SAML Logout Handler do Single Logout when session is terminated.
+ */
 
 public class SAMLLogoutListener extends AbstractEventHandler {
 
@@ -42,7 +62,7 @@ public class SAMLLogoutListener extends AbstractEventHandler {
                 try {
                     samlSsoService.doSingleLogout(samlssoTokenId, serviceProvider);
                 } catch (IdentityException e) {
-                    log.error("Error while doing single logout in SAML.", e);
+                    log.error("Error while doing single logout for " + serviceProvider + ".", e);
                 }
             }
         }
