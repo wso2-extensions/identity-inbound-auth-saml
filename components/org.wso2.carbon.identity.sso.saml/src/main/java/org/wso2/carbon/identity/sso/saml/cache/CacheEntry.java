141632be3ddd85b138c94c4a8cc49064a4961735
nuwandiw <nuwandiw@wso2.com>
2018-06-07 11:21:09 +0530

    Set temporary data cleanup timeout for saml session context

--- components/org.wso2.carbon.identity.sso.saml/src/main/java/org/wso2/carbon/identity/sso/saml/cache/CacheEntry.java
+++ components/org.wso2.carbon.identity.sso.saml/src/main/java/org/wso2/carbon/identity/sso/saml/cache/CacheEntry.java
@@ -18,13 +18,11 @@
 
 package org.wso2.carbon.identity.sso.saml.cache;
 
-import java.io.Serializable;
-
 /**
  * Represents a cache entry. Any value that needs to be included in the
  * cache must be encapsulated with a class deriving from this class.
  */
-public abstract class CacheEntry implements Serializable {
+public abstract class CacheEntry extends org.wso2.carbon.identity.application.common.cache.CacheEntry {
 
     private static final long serialVersionUID = -269579000793589533L;
 }
