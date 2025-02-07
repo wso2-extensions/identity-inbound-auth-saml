c09e54856c64267c5b48c54e44f4ea34b81d4682
DilshanSenarath <74205483+DilshanSenarath@users.noreply.github.com>
2025-02-07 08:58:11 +0530

    update copyright header

--- components/org.wso2.carbon.identity.sso.saml/src/main/java/org/wso2/carbon/identity/sso/saml/processors/IdPInitSSOAuthnRequestProcessor.java
+++ components/org.wso2.carbon.identity.sso.saml/src/main/java/org/wso2/carbon/identity/sso/saml/processors/IdPInitSSOAuthnRequestProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) (2010-2023), WSO2 LLC. (http://www.wso2.com).
+ * Copyright (c) 2010-2025, WSO2 LLC. (https://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -15,6 +15,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.wso2.carbon.identity.sso.saml.processors;
 
 import org.apache.commons.lang.StringUtils;
